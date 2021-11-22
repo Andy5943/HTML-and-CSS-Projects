2a675501f69e471387c86654dab4699b569d6b78
Andy5943 <92390768+Andy5943@users.noreply.github.com>
2021-11-22 09:12:14 -0600

    Add files via upload

---
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML>
+<html>
+<script>
+alert("hello world!");
+function My_First_function() {
+		var String = "Kiss me, I'm Irish!";
+		var result = String.fontcolor("green");
+		document.getElementById("Irish").innerHTML =
+		result;
+	} 
+</script>
+</html>
